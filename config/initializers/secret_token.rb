5ba3dbdc24fc676fe2fc4c532a4b7d0f929a2ff9
Liji <desanlee@localhost.(none)>
2013-09-29 23:01:59 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refweibo::Application.config.secret_token = 'ef7ef10774c218652487d6de8747f6498f322a1c91be2feebe1300056f538ec18ce68cc47c845fb2a84482dbd3f31af2bd4d9ee3cc46dfede46598c3bad1fe79'
